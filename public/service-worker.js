e3b93e7e05d13c08542723dd1b157c444341df66
Portmeister <portmeister@gmail.com>
2021-02-25 12:50:07 -0600

    updating

--- public/service-worker.js
+++ public/service-worker.js
@@ -5,7 +5,7 @@ const FILES_TO_CACHE = [
     "/assets/js/index.js",
     "/manifest.webmanifest",
     "/assets/images/icons/icon-192x192.png",
-    "/assets/images/icons/icon-512x512.png"
+    "/assets/images/icons/icon-512x512.png",
   ];
   
   const CACHE_NAME = "static-cache-v2";
